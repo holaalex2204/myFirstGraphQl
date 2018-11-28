34a43a7ddac356083b1ff281f7888ab1d391c190
Alejandro Ortiz <alejandroortiz@generation.org>
2018-11-28 11:11:48 -0600

    First version

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const { graphql, buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+# Defines all the things that we can "start" with
+# Describes what is possibly, and perhaps more importantly what's not possible
+type Query {
+  foo: RecruitmentProcess
+}
+type RecruitmentProcess {
+  numberApplicants: Int,
+  name: String
+}
+type Schema {
+  query: Query
+}
+`);
+
+const resolvers = {
+  RecruitmentProcess_numberApplicants: () => 10,
+  RecruitmentProcess_name: () => 'Work@LaMaquinista',
+};
+
+const query = `{ 
+  foo {
+    name
+  }
+}`;
+
+/**
+ * graphql(
+ *   schema: GraphQLSchema,
+ *   requestString: string,
+ *   rootValue?: ?any, passed as root value to the executor
+ *   contextValue?: ?any,
+ *   variableValues?: ?{[key: string]: any},
+ *   operationName?: ?string
+ * ): Promise<GraphQLResult>
+ */
+graphql(schema, query, resolvers)
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
